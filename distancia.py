a705d765816eef773899d6f94496e841807e46af
StephanGuingor <stephan.guingor04@gmail.com>
2020-09-18 11:11:56 -0500

    Added Graphs

--- distancia.py
+++ distancia.py
@@ -3,7 +3,7 @@ Devuelve la distancia euclideana entre dos klistas
 """
 #distancia.py
 from scipy.spatial import distance
-
+# np.sqrt(np.sum((np.array(b)-np.array(a))**2))
 def distancia(a,b):
     dist = distance.euclidean(a, b)
     return dist
--- main.py
+++ main.py
@@ -12,9 +12,11 @@ if __name__ == "__main__":
                            cluster_std=0.60, random_state=0)
 
 
+
     # KMeans Via Us
     # cent = kmeans(X,k=4)
-    km = KMeansTec(n_clusters=4)
+    k = 4
+    km = KMeansTec(n_clusters=k)
     km.fit(X)
     y_kmeans = km.predict(X)
     # print(y_kmeans)
@@ -22,21 +24,22 @@ if __name__ == "__main__":
     plt.grid(True)
     plt.title("Nuestro")
     plt.scatter(X[:, 0], X[:, 1], s=50, c=y_kmeans,cmap='coolwarm');
-    plt.scatter([c[0] for c in km.centers ], [c[1] for c in km.centers ], c='black', s=200, alpha=0.5);
+    plt.scatter([c[0] for c in km.centers ], [c[1] for c in km.centers ], c='black', s=200, alpha=0.5)
 
 
     # Sklearn KMeans
     from sklearn.cluster import KMeans
-    kmeans = KMeans(n_clusters=4)
+    kmeans = KMeans(n_clusters=k)
     kmeans.fit(X)
     y_kmeans = kmeans.predict(X)
     # print(y_kmeans)
     plt.subplot(122)
     plt.title("Sklearn")
+    plt.grid(True)
+    # Plot Clusters
     plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
     centers = kmeans.cluster_centers_
-    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 
     pprint(f"Sklearn -> {centers} |\n US -> {km.centers}")
 
